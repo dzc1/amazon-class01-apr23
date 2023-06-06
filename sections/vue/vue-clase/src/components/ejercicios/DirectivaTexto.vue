bc57cebbc26405dae2863174e0b8512eb6ac8523
Diego Zito <diegozito.m@gmail.com>
2023-06-06 22:09:15 +0200

    vue class day 001 -- excercises folder uploaded

---
+++ sections/vue/vue-clase/src/components/ejercicios/DirectivaTexto.vue
@@ -0,0 +1,34 @@
+<!-- html aca -->
+<template>
+  <!-- Ejemplo de como se veria -->
+  <div>
+    Hola mi nombre es Diego Z, y tengo 32 años. Actualmente vivo en Barcelona,
+    España. Soy nativo de Valencia, Venezuela y naci el 16 de mayo de 1991. Mi
+    banda preferida es/son Red Hot Chilli Peppers pero mi ultimo concierto fue
+    Kendrick Lamar i
+  </div>
+  <!-- Tu trabajo -->
+  <div></div>
+</template>
+
+<!-- Pongan su JS aca -->
+<script setup>
+// Creanse un objeto reactivo que contenga la siguiente informacion
+// nombre
+// edad
+// Año de nacimiento
+// Ciudad actual
+// Pais Actual
+// Ciudad Natal
+// Pais Natal
+// banda preferida
+// Ultimo concierto
+
+// Objetivo
+// Impriman dentro de un solo elemento tipo p todos estos valores del objeto mediante el directivo v-text
+// Ayuda
+// Recuerdate del elemento tipo linea <span>
+</script>
+
+<!-- CSS con atributo scoped aca -->
+<style scoped></style>
